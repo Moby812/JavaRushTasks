3367f3cb7372904b3770a2d787f83f5ea489267f
Moby812 <moby812@gmail.com>
2019-12-28 20:43:11 +0300

    использование return

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -7,11 +7,13 @@ package com.javarush.task.task03.task0303;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+        System.out.println(convertEurToUsd(10,1.12));   //к курсу $
+        System.out.println(convertEurToUsd(10,70.59));  //к курсу руб
     }
 
     public static double convertEurToUsd(int eur, double course) {
         //напишите тут ваш код
 
+        return eur*course;
     }
 }

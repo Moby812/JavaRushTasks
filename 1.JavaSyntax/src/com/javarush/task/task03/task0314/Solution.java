75eef2ee587456b5861ae3ceaa36b5278fdf385c
Парамонов Павел <moby812@gmail.com>
2019-12-30 12:03:01 +0300

    Вывести таблицу умножения

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0314;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}

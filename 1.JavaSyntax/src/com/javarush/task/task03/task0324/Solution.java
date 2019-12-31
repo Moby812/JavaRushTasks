93dd3c8076fdf84caf061ffc638d12cea8c70618
Парамонов Павел <moby812@gmail.com>
2019-12-31 11:41:33 +0300

    Вывод строки 10 раз используя цикл while

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0324;
+
+/* 
+Меркантильные намерения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int count = 0;
+        while (count < 10) {                                                                  //пока это условие true - будет выполнять тело цикла
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+            count++;
+        }
+    }
+}

e37d03c7be4b29ab61a0c4050e007178bd233166
Парамонов Павел <moby812@gmail.com>
2019-12-31 07:59:10 +0300

    Сканер с преобразованием строки в число

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task03.task0319;
+
+/* 
+Предсказание на будущее
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));   //инициализируем сканер
+        String name = reader.readLine();                                                //задаем чтение с клавы
+        String number1 = reader.readLine();
+        String number2 = reader.readLine();
+        int number1F = Integer.parseInt(number1);                                       //преобразуем строку в число
+        int number2F = Integer.parseInt(number2);
+
+        System.out.println(name+" получает "+number1F+" через "+number2F+" лет.");
+    }
+}

5106fe70796b76d4c3767412b963a37dcc02f8e1
Moby812 <moby812@gmail.com>
2020-01-01 11:36:13 +0300

    Integer.parsentInt(reader.readLine());

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0325;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+Финансовые ожидания
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int money = Integer.parseInt(reader.readLine());
+        System.out.println("Я буду зарабатывать $"+money+" в час");
+    }
+}

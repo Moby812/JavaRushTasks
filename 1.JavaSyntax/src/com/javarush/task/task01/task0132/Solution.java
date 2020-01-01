2822eba0d830e3a7314ef5399b8d719033b41bb8
Moby812 <moby812@gmail.com>
2020-01-01 12:03:54 +0300

    % --остаток от деления (546%100 = 46%10 = 6)

--- 1.JavaSyntax/src/com/javarush/task/task01/task0132/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0132/Solution.java
@@ -11,9 +11,9 @@ public class Solution {
 
     public static int sumDigitsInNumber(int number) {
         //напишите тут ваш код
-        int number1 = number/100;
-        int number2 = (number%100)/10;
-        int number3 = number%10;
-        return number1+number2+number3;
+        int number1 = number / 100;
+        int number2 = (number % 100) / 10;
+        int number3 = number % 10;
+        return number1 + number2 + number3;
     }
 }

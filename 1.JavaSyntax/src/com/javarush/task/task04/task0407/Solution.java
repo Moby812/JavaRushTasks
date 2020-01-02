6ee3f4a851457f8fe595d8124e22d0f251fc33e6
Moby812 <moby812@gmail.com>
2020-01-02 16:13:16 +0300

    Правильно считаем количество созданных объектов и выводим на экран правильное их количество.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0407/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0407;
+
+/* 
+Кошки во Вселенной
+ правильно считать количество созданных котов (count) и на экран выдавать правильное их количество.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        //напишите тут ваш код
+        Cat.count++;
+
+        Cat cat2 = new Cat();
+        //напишите тут ваш код
+        Cat.count++;
+
+        System.out.println("The cat count is " + Cat.count);
+    }
+
+    public static class Cat {
+        public static int count = 0;
+    }
+}

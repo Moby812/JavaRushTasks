857a2e2d6dc74ceb71552ebdb23440c5ff14c632
Moby812 <moby812@gmail.com>
2020-01-02 19:00:55 +0300

    Указание размера игрового поля

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -2,4 +2,9 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game{
+    private static final int SIDE=9;        //размер поля
+    @Override
+    public void initialize(){
+        setScreenSize(SIDE,SIDE);
+    }
 }

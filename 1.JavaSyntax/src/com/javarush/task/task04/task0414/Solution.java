03daa740019a343c32efd8d9b018f3f52968abd7
Moby812 <moby812@gmail.com>
2020-01-04 13:39:38 +0300

    заготовка под календарь

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task04.task0414;
+
+/* 
+Количество дней в году
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(reader.readLine());
+        String text ="количество дней в году: ";
+
+
+    }
+}

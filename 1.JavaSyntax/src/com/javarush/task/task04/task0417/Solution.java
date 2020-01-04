e013e004d2a779e5be0605b1966462039f29dd93
Moby812 <moby812@gmail.com>
2020-01-04 12:16:31 +0300

    Набросок на поиск пары

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task04.task0417;
+
+/* 
+Существует ли пара?
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number1 = Integer.parseInt(reader.readLine());
+        int number2 = Integer.parseInt(reader.readLine());
+        int number3 = Integer.parseInt(reader.readLine());
+
+        if (number1 == number2 || number1 == number3) {
+            System.out.println(number1);
+        } else if (number2 == number1 || number2 == number3) {
+            System.out.println(number2);
+        } else if (number3 == number1 || number3 == number2) {
+            System.out.println(number3);
+        }
+    }
+}

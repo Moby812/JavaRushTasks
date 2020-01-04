c02fb18d983a9e31f962a3f5cd849d00fd2428e3
Moby812 <moby812@gmail.com>
2020-01-04 16:26:26 +0300

    Заготовка под светофор

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task04.task0416;
+
+/* 
+Переходим дорогу вслепую
+в начале каждого часа в течение трех минут горит зелёный сигнал,
+затем в течение одной минуты - жёлтый,
+а потом в течение одной минуты - красный,
+затем опять зелёный горит три минуты и т. д.
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double t = Integer.parseInt(reader.readLine());
+        String green = "зелёный";
+        String yellow = "жёлтый";
+        String red = "красный";
+        int hour=60;
+
+        if (t ==) {
+            System.out.println(green);
+        } else if (t ==) {
+            System.out.println(yellow);
+        } else {
+            System.out.println(red);
+        }
+    }
+}

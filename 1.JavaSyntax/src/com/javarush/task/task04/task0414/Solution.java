e504e9af8545cfe91a938dc34a345ea5c433e459
Moby812 <moby812@gmail.com>
2020-01-04 15:52:55 +0300

    заготовка под календарь

--- 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -12,12 +12,16 @@ public class Solution {
         //напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int year = Integer.parseInt(reader.readLine());
-        String text ="количество дней в году: ";
+        String text = "количество дней в году: ";
         int leapYear = 366;         //висакосны год
         int standardYear = 365;     //не висакосный год
 
-        if ((year/400) == 0) {
-            System.out.println(text+leapYear);
+        if (year % 400 == 0 || year % 400 % 100 % 4 == 0) {
+            System.out.println(text + leapYear);
+        } else if (year % 400 % 100 == 0) {
+            System.out.println(text + standardYear);
+        } else {
+            System.out.println(text + standardYear);
         }
     }
 }

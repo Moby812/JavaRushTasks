97870a4025ea67161bc7b056a48e71b89565348f
Moby812 <moby812@gmail.com>
2020-01-05 14:01:53 +0300

    Определяем координаторную четверть

--- 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
@@ -14,5 +14,14 @@ public class Solution {
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
 
+        if (a > 0 && b > 0) {
+            System.out.println(1);
+        } else if (a < 0 && b > 0) {
+            System.out.println(2);
+        } else if (a < 0 && b < 0) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
     }
 }

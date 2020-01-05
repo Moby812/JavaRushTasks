8462c1cd5f1aa0c74a7a21de56050910e2260a1b
Moby812 <moby812@gmail.com>
2020-01-05 11:56:08 +0300

    заготовка под задачу

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0425;
+
+/* 
+Цель установлена!
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+    }
+}

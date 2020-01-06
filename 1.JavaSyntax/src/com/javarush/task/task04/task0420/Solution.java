1a00664670f72a6612176b03d3c98be8335cb64d
Moby812 <moby812@gmail.com>
2020-01-06 12:25:27 +0300

    Сортировка 3х чисел

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task04.task0420;
+
+/* 
+Сортировка трех чисел
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+    }
+}

57b485149cab373751353d77e13eae1f8538d1b7
Moby812 <moby812@gmail.com>
2020-01-07 09:22:25 +0300

    Считаем количество положительных чисел

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0428/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0428;
+
+/* 
+Положительное число
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+        int num3 = Integer.parseInt(reader.readLine());
+
+        int counter = 0;
+        if (num1 > 0) counter++;
+        if (num2 > 0) counter++;
+        if (num3 > 0) counter++;
+        System.out.println(counter);
+    }
+}

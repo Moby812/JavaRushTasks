e280c9be164cebd21319303e9eab91e6c07f173d
Moby812 <moby812@gmail.com>
2020-01-08 10:30:01 +0300

    Считаем от 1 до 10

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0430/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task04.task0430;
+
+/* 
+От 1 до 10
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int count = 0;
+        while (count < 10) {
+            count++;
+            System.out.println(count);
+        }
+    }
+}

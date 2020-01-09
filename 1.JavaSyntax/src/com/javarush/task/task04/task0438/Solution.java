f4bd13affbbc2757595d110d4e5d1fef1dc11f02
Парамонов Павел <moby812@gmail.com>
2020-01-09 07:24:05 +0300

    Используя цикл for выводим на экран горизонтальную/верткальную линию из 8

--- 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -8,8 +8,8 @@ public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
 
-        for (int i = 0; i < 10; i++) System.out.print(8);
-        for (int i = 0; i < 10; i++) System.out.println(8);
+        for (int i = 0; i <= 8; i++) System.out.print(8);
+        for (int i = 0; i <= 10; i++) System.out.println(8);
 
     }
 }

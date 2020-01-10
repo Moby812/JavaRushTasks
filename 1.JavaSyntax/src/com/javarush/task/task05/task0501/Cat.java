40a4c1fe872003411b0a64c5a0073762bfe84d8d
Парамонов Павел <moby812@gmail.com>
2020-01-10 09:09:19 +0300

    Объявляем переменные классу

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task05.task0501;
+
+/* 
+Создание кота
+Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    String name;
+    int age;
+    int weight;
+    int strength;
+
+    public static void main(String[] args) {
+
+    }
+}

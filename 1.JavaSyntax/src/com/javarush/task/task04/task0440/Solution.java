f200110a3ecc786e4cb6e717811335ebd56d6531
Парамонов Павел <moby812@gmail.com>
2020-01-10 06:43:27 +0300

    Через цикл, 100 раз выводим надпись

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0440/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task04.task0440;
+
+/* 
+Достойная оплата труда
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String text = "Я никогда не буду работать за копейки. Амиго";
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(text);
+        }
+    }
+}

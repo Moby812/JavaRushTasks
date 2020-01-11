db5d24100a71cdc416227bf9ff64a16ce7712371
Moby812 <moby812@gmail.com>
2020-01-11 16:07:04 +0300

    Геттеры и сеттеры для класса

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task05.task0503;
+
+
+/* 
+Геттеры и сеттеры для класса Dog
+Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+*/
+
+public class Dog {
+    //напишите тут ваш код
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+    public void setName (String name) {
+        this.name=name;
+    }
+
+    public int getAge () {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age=age;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

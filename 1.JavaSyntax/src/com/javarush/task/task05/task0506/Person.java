3370a49c15e0e23475dd8bc662513899f9eddf49
Moby812 <moby812@gmail.com>
2020-01-11 15:45:47 +0300

    Добавляем переменные классу

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0506/Person.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task05.task0506;
+
+/* 
+Человечки
+Создать class Person. У человека должно быть имя String name, возраст int age, адрес String address, пол char sex.
+*/
+
+public class Person {
+    //напишите тут ваш код
+    String name;
+    int age;
+    String address;
+    char sex;
+
+    public static void main(String[] args) {
+
+    }
+}

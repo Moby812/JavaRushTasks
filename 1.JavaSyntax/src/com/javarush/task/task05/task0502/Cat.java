b56db2b2622078012fa5bf7661adf36e985a0094
Moby812 <moby812@gmail.com>
2020-01-11 15:39:56 +0300

    Реализуем метод public boolean fight, который будет считать выигровшего бой кота + вариант реализации через сравнительный метод Integer.compare()

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -2,15 +2,6 @@ package com.javarush.task.task05.task0502;
 
 /* 
 Реализовать метод fight
-
-Реализовать метод boolean fight(Cat anotherCat):
-реализовать механизм драки котов в зависимости от их веса, возраста и силы.
-Нужно сравнить каждый критерий по отдельности, и победителем будет тот, у которого больше "победивших" критериев.
-Метод должен определять, выиграли ли мы (this) бой или нет, т.е. возвращать true, если выиграли и false - если нет.
-Если ничья и никто не выиграл, возвращаем либо true либо false, но должно выполняться условие:
-если cat1.fight(cat2) возвращает true,
-то cat2.fight(cat1) должен возвращать false.
-
 */
 
 public class Cat {
@@ -21,7 +12,7 @@ public class Cat {
     public Cat() {              //пустой конструктор
     }
 
-//    public Cat(int age, int weight, int strength) {
+//    public Cat(int age, int weight, int strength) {           //конструктор в который передаём параметры
 //        this.age = age;
 //        this.weight = weight;
 //        this.strength = strength;
@@ -46,40 +37,28 @@ public class Cat {
         int winCat1 = 0;
         int winCat2 = 0;
 
-        if (this.age > anotherCat.age)
-            winCat1++;
-        else
-            winCat2++;
+        if (this.age > anotherCat.age) winCat1++;
+        else if (this.age < anotherCat.age) winCat2++;
 
-        if (this.weight > anotherCat.weight)
-            winCat1++;
-        else
-            winCat2++;
+        if (this.weight > anotherCat.weight) winCat1++;
+        else if (this.weight < anotherCat.weight) winCat2++;
 
-        if (this.strength > anotherCat.strength)
-            winCat1++;
-        else
-            winCat2++;
+        if (this.strength > anotherCat.strength) winCat1++;
+        else if (this.strength < anotherCat.strength) winCat2++;
 
         return winCat1 > winCat2;
     }
 
     public static void main(String[] args) {
 
-//    Cat cat1 = new Cat(5, 15,5);
-//    Cat cat2 = new Cat(3,13,4);
-
         Cat cat1 = new Cat();
-        cat1.age = 5;
-        cat1.weight = 15;
-        cat1.strength = 5;
+        cat1.age = 10;
+        cat1.weight = 1;
+        cat1.strength = 1;
 
         Cat cat2 = new Cat();
-        cat2.age = 3;
-        cat2.weight = 13;
-        cat2.strength = 4;
-
-        System.out.println(cat1.fight(cat2));
-        System.out.println(cat2.fight(cat1));
+        cat2.age = 1;
+        cat2.weight = 1;
+        cat2.strength = 1;
     }
 }

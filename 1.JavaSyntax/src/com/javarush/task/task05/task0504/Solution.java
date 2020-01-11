3b6fcaaaebdd73818460888fa6561b89d45243cd
Moby812 <moby812@gmail.com>
2020-01-11 15:05:22 +0300

    Создаём 3 объекта по конструктору

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat tom = new Cat("Tom", 5,8,3);
+        Cat rom = new Cat("Rom", 13,2,8);
+        Cat kom = new Cat("Kom", 1,80,30);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}

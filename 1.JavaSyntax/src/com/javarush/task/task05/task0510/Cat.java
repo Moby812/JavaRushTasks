27a2e9e0a30c4140f3d528a0f065272ca019f3e4
Moby812 <moby812@gmail.com>
2020-01-12 13:03:45 +0300

    Задачка с котами. перезагрузка метода initialize()

--- 1.JavaSyntax/src/com/javarush/task/task05/task0510/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0510/Cat.java
@@ -6,41 +6,41 @@ package com.javarush.task.task05.task0510;
 
 public class Cat {
     //напишите тут ваш код
-    public String name,address,color;
+    public String name, address, color;
     public int age, weight;
 
-    public void initialize(Cat name){   //принимающий в качестве параметра имя, но инициализирующий все переменные класса, кроме адреса.
-        this.name=name.name;
-        this.age=age;
-        this.weight=weight;
-        this.color=color;
+    public void initialize(String name) {   //принимающий в качестве параметра имя, но инициализирующий все переменные класса, кроме адреса.
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
     }
 
-    public void initialize(Cat name, Cat weight, Cat age){  //принимающий в качестве параметров имя, вес, возраст и инициализирующий все переменные класса, кроме адреса.
-        this.name=name.name;
-        this.age=age.age;
-        this.weight=weight.weight;
-        this.color=color;
+    public void initialize(String name, int weight, int age) {  //принимающий в качестве параметров имя, вес, возраст и инициализирующий все переменные класса, кроме адреса.
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
     }
 
-    public void initialize(Cat name, Cat age){  //принимающий в качестве параметров имя, возраст и инициализирующий все переменные класса, кроме адреса.
-        this.name=name.name;
-        this.age=age.age;
-        this.weight=weight;
-        this.color=color;
+    public void initialize(String name, int age) {  //принимающий в качестве параметров имя, возраст и инициализирующий все переменные класса, кроме адреса.
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
     }
 
-    public void initialize(Cat weight, Cat color) {  //принимающий в качестве параметров вес, цвет и инициализирующий все переменные класса, кроме имени и адреса.
-        this.age=age;
-        this.weight=weight.weight;
-        this.color=color.color;
+    public void initialize(int weight, String color) {  //принимающий в качестве параметров вес, цвет и инициализирующий все переменные класса, кроме имени и адреса.
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
     }
 
-    public void initialize(Cat weight, Cat color, Cat address){   //принимающий в качестве параметров вес, цвет, адрес и инициализирующий все переменные класса, кроме имени.
-        this.age=age;
-        this.weight=weight.weight;
-        this.color=color.color;
-        this.address=address.address;
+    public void initialize(int weight, String color, String address) {   //принимающий в качестве параметров вес, цвет, адрес и инициализирующий все переменные класса, кроме имени.
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
     }
 
     public static void main(String[] args) {

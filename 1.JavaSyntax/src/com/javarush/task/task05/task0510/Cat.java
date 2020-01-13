4117a90aa60fd90badc31d649854f04f7ea77865
Moby812 <moby812@gmail.com>
2020-01-13 18:26:46 +0300

    Задачка с котами. перезагрузка метода initialize()

--- 1.JavaSyntax/src/com/javarush/task/task05/task0510/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0510/Cat.java
@@ -6,38 +6,39 @@ package com.javarush.task.task05.task0510;
 
 public class Cat {
     //напишите тут ваш код
-    public String name, address, color;
-    public int age, weight;
+    private String name, address, color;
+    private int age, weight;
 
     public void initialize(String name) {   //принимающий в качестве параметра имя, но инициализирующий все переменные класса, кроме адреса.
         this.name = name;
-        this.age = age;
-        this.weight = weight;
-        this.color = color;
+        this.age = 5;
+        this.weight = 5;
+        this.color = "black";
     }
 
     public void initialize(String name, int weight, int age) {  //принимающий в качестве параметров имя, вес, возраст и инициализирующий все переменные класса, кроме адреса.
         this.name = name;
         this.age = age;
         this.weight = weight;
-        this.color = color;
+        this.color = "black";
     }
 
+    //успешеная валидация
     public void initialize(String name, int age) {  //принимающий в качестве параметров имя, возраст и инициализирующий все переменные класса, кроме адреса.
         this.name = name;
         this.age = age;
-        this.weight = weight;
-        this.color = color;
+        this.weight = 5;
+        this.color = "black";
     }
 
     public void initialize(int weight, String color) {  //принимающий в качестве параметров вес, цвет и инициализирующий все переменные класса, кроме имени и адреса.
-        this.age = age;
+        this.age = 5;
         this.weight = weight;
         this.color = color;
     }
 
     public void initialize(int weight, String color, String address) {   //принимающий в качестве параметров вес, цвет, адрес и инициализирующий все переменные класса, кроме имени.
-        this.age = age;
+        this.age = 5;
         this.weight = weight;
         this.color = color;
         this.address = address;

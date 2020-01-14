31fb4277eb5485c88d022a090a6944eaf9fded2e
Парамонов Павел <moby812@gmail.com>
2020-01-14 15:54:52 +0300

    Выводим на экран минимальное из 5-ти чисел

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task05.task0531;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Совершенствуем функциональность
+Программа вводит два числа с клавиатуры и выводит минимальное из них на экран.
+Новая задача: Программа вводит пять чисел с клавиатуры и выводит минимальное из них на экран.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+//        int minimum = min(e, min(min(a, b), min(c, d)));
+        int minimum = min(a, min(b, min(c, min(d, e))));
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}

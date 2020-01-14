c308b7555273e7a969fbd56dbed8439361bed2ac
Парамонов Павел <moby812@gmail.com>
2020-01-14 14:08:58 +0300

    Создал 2 класса, 2 коструктора и 2 объекта

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task05.task0527;
+
+/* 
+Том и Джерри
+1. Создай класс Dog.
+2. Создай класс Cat.
+3. В классе Dog должно быть три переменные.
+4. В классе Cat должно быть три переменные.
+5. Должен быть создан хотя бы один объект типа Mouse.
+6. Должен быть создан хотя бы один объект типа Dog.
+7. Должен быть создан хотя бы один объект типа Cat.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+
+        //напишите тут ваш код
+        Dog spikeDog = new Dog("Spike",10,1);
+        Cat tomCat = new Cat("Tom",5,2);
+    }
+
+    public static class Mouse {
+        String name;
+        int height;
+        int tail;
+
+        public Mouse(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    //напишите тут ваш код
+    public static class Dog {
+        String name;
+        int height, tail;
+
+        public Dog(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Cat {
+        String name;
+        int height, tail;
+
+        public Cat(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+}

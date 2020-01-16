2cdaec9d42710d3fa6bc7a7717291c92df437a12
Moby812 <moby812@gmail.com>
2020-01-16 18:35:54 +0300

    Сделал таймер, время на который выставляется с клавиатуры

--- 1.JavaSyntax/src/com/javarush/task/task06/Timer.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/Timer.java
@@ -14,40 +14,27 @@ public class Timer {
         Calendar nowTime = new GregorianCalendar();
         Calendar needTime = new GregorianCalendar();
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
-        System.out.println("Сейчас ровно: "+ dateFormat.format(nowTime.getTime()));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        System.out.println("Сейчас ровно: " + dateFormat.format(nowTime.getTime()));
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введи в минутах, на сколько выставить таймер..");
+        System.out.println("Введи в минутах, на сколько выставить таймер..\n");
         int timer = Integer.parseInt(reader.readLine());                                                                // считаем число с клавы
         int count = timer;
 
         needTime.add(Calendar.MINUTE, timer);           //выставляем время. (Текущее + которые мы задали с клавы)
-        System.out.println("Таймер выставлен на: "+ count +" минут.\nДо: "+ dateFormat.format(needTime.getTime()) +"\nОтсчёт пошёл!\n\n"); // TODO: 2020-01-16 сделать чтоб выводило когда сработает таймер!
+        System.out.println("Таймер выставлен на: " + count + " минут(ы).\nДо: " + dateFormat.format(needTime.getTime()) + "\nОтсчёт пошёл!\n\n");
 
         while (true) {
-//            if (nowTime.getTime().equals(needTime.getTime())) {
-            if (nowTime.getTime().after(needTime.getTime())) {
+            Calendar now = new GregorianCalendar();
+            if (now.getTime().after(needTime.getTime())) {
                 System.out.println("Настало время приключений!!");
                 break;
-            } else {
-                Thread.sleep(60100);                 //приостановим работу программы на 60 секунд
+            } else {    // TODO: 16.01.2020 сделать проверку по условию ветвленияБ чтоб отображал в N сек - считывая с даты
+                Thread.sleep(60000);                 //приостановим работу программы на 60 секунд
                 count--;
-                System.out.println("Осталось подождать ещё " + count + " минут");
+                if (count != 0) System.out.println("Осталось подождать ещё " + count + " минут(ы)");
             }
         }
-
- /*
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int timer = Integer.parseInt(reader.readLine());
-        LocalDateTime finishTime = LocalDateTime.now().plusMinutes(timer);
-        while(LocalDateTime.now().isBefore(finishTime)){
-            if(LocalDateTime.now().getSecond() == 0 && LocalDateTime.now().getNano() == 0){
-                System.out.println(LocalDateTime.now());
-            }
-        }
-        System.out.println("timeout");
-
-         */
     }
 }

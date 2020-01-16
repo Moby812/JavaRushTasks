095152876d78de91138a1a3b427816fff8e0393b
Парамонов Павел <moby812@gmail.com>
2020-01-16 13:54:40 +0300

    Метод finalize класса выводит на экран строку.

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0602/Cat.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task06.task0602;
+
+/* 
+Пустые кошки, пустые псы
+4. Метод finalize класса Cat должен выводить на экран "A Cat was destroyed".
+5. Метод finalize класса Dog должен выводить на экран "A Dog was destroyed".
+*/
+
+public class Cat {
+    public static void main(String[] args) {
+
+    }
+
+    //напишите тут ваш код
+    protected void finalize(){
+        System.out.println("A Cat was destroyed");
+    }
+}
+
+class Dog {
+    //напишите тут ваш код
+    protected void finalize(){
+        System.out.println("A Dog was destroyed");
+    }
+}

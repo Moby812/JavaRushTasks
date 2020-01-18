bfd87660020365a7ce364f20b42e2b1a4f95c6f0
Moby812 <moby812@gmail.com>
2020-01-18 17:00:09 +0300

    Считываем с консоли число N,потом считываем N чисел с консоли и выводим на экран максимальное из введенных N чисел.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -17,19 +17,19 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(reader.readLine());
-        if (count > 0) {
-            int maximum = 0;
+        int maximum = Integer.MIN_VALUE;
 
+        if (count > 0) {
             //напишите тут ваш код
             while (count > 0) {
                 int n = Integer.parseInt(reader.readLine());
 //                if (n > 0) System.out.println(n);
-                maximum = n > maximum ? n : maximum;    // аналог -- if (n > maximum) maximum = n;
-//            maximum = Math.max(n, maximum);
+//                maximum = n > maximum ? n : maximum;    // аналог -- if (n > maximum) maximum = n;
+                maximum = Math.max(n, maximum);
                 count--;
             }
-
-            if (maximum > 0) System.out.println(maximum);
         }
+        if (maximum > 0) System.out.println(maximum);
+//        System.out.println(maximum);
     }
 }

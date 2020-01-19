8c461cf2c3d009b0eebe0de5e53506c0dd1080ff
Moby812 <moby812@gmail.com>
2020-01-19 13:26:54 +0300

    ИМТ или BMI

--- 1.JavaSyntax/src/com/javarush/task/task06/BMI.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/BMI.java
@@ -27,17 +27,13 @@ public class BMI {
             double idealMinWeight;    //в зависимости от роста считаем нижнюю планку идеального веса    //граница 18.5
             idealMinWeight = 18.5 * (height * height);
             double addIdealMinWeight = idealMinWeight - weight;
+            System.out.println("Нормальный вес при таком росте: "+idealMinWeight+" - "+idealMaxWeight+"кг.");
             System.out.format(Color.WHITE + "Твой ИМТ: "+"%.1f%n", mass);  //привести к формату --http://study-java.ru/uroki-java/formatirovanie-chisel-i-texta-v-java/
-            System.out.println("Верхняя граница идеального веса при таком росте = "+idealMaxWeight);
-            System.out.println("До неё надо скинуть ещё "+minusIdealMaxWeight);
-            System.out.println("Нижняя граница идеального веса при таком росте = "+idealMinWeight);
-            System.out.println("До неё надо набрать ещё "+addIdealMinWeight);
 
-
-            if (mass < 18.5) System.out.println(Color.YELLOW + "Недовес: меньше чем 18.5");
+            if (mass < 18.5) System.out.println(Color.YELLOW + "Недовес: меньше чем 18.5 \nДо нормального веса надо набрать хотя бы: "+addIdealMinWeight);
             else if (mass >= 18.5 && mass < 25) System.out.println(Color.GREEN + "Нормальный: между 18.5 и 25");
-            else if (mass >= 25 && mass < 30) System.out.println(Color.YELLOW + "Избыточный вес: между 25 и 30");
-            else System.out.println(Color.RED + "Ожирение, от 30 и выше. \nДо нормального веса надо скинуть хотя бы: " );
+            else if (mass >= 25 && mass < 30) System.out.println(Color.YELLOW + "Избыточный вес: между 25 и 30 \nДо нормального веса надо скинуть хотя бы: "+minusIdealMaxWeight);
+            else System.out.println(Color.RED + "Ожирение: от 30 и выше. \nДо нормального веса надо скинуть хотя бы: "+minusIdealMaxWeight);
         }
 
         private static class Color {

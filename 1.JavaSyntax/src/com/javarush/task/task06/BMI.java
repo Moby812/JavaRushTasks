833d34057da57a312c97360cd5ed0a441f488924
Moby812 <moby812@gmail.com>
2020-01-19 11:55:42 +0300

    ИМТ или BMI

--- 1.JavaSyntax/src/com/javarush/task/task06/BMI.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/BMI.java
@@ -21,7 +21,7 @@ public class BMI {
     public static class Body {
         public static void BMI(double weight, double height) {
             double mass = weight / (height * height);
-            System.out.println(Color.WHITE + "Твой ИМТ: " + mass);
+            System.out.format(Color.WHITE + "Твой ИМТ: "+"%.1f%n", mass);  //привести к формату --http://study-java.ru/uroki-java/formatirovanie-chisel-i-texta-v-java/
             if (mass < 18.5) System.out.println(Color.YELLOW + "Недовес: меньше чем 18.5");
             else if (mass >= 18.5 && mass < 25) System.out.println(Color.GREEN + "Нормальный: между 18.5 и 25");
             else if (mass >= 25 && mass < 30) System.out.println(Color.YELLOW + "Избыточный вес: между 25 и 30");
@@ -29,13 +29,9 @@ public class BMI {
         }
 
         private static class Color {
-//            static final String BLACK = "\033[1;30m";
             static final String RED = "\033[1;31m";
             static final String GREEN = "\033[1;32m";
-//            static final String BLUE = "\033[1;34m";
             static final String YELLOW = "\033[1;33m";
-//            static final String PURPLE = "\033[1;35m";
-//            static final String CYAN = "\033[1;36m";
             static final String WHITE = "\033[1;37m";
         }
     }

90cf5f99cca741b6814887be4063e5524b69e1c0
Moby812 <moby812@gmail.com>
2020-01-20 06:06:40 +0300

    Конструктор увеличивает значение статической переменной catCount на 1.

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0613/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task06.task0613;
+
+/* 
+Кот и статика
+В классе Cat создай статическую переменную public int catCount.
+Создай конструктор [public Cat()]. Пусть при каждом создании кота (нового объекта Cat) статическая переменная catCount увеличивается на 1.
+Создать 10 объектов Cat и вывести значение переменной catCount на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создай 10 котов
+        for (int i = 0; i < 10; i++) {
+            Cat cat = new Cat();
+        }
+
+        // Выведи значение переменной catCount
+        System.out.println(Cat.catCount);
+    }
+
+    public static class Cat {
+        // Создай статическую переменную catCount
+        public static int catCount = 0;
+        // Создай конструктор
+        public Cat(){
+            catCount++;
+        }
+    }
+}

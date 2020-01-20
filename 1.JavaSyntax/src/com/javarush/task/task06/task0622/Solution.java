604c3a290d7a5483a1fc233268dc99c04fab2ca9
Moby812 <moby812@gmail.com>
2020-01-20 19:56:50 +0300

    Числа по возрастанию. Перебор массивом и листом

--- 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.task.task06.task0622;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /* 
 Числа по возрастанию
@@ -12,22 +14,30 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        //напишите тут ваш код
+        //напишите тут ваш код  http://study-java.ru/uroki-java/urok-11-sortirovka-massiva/
         int number1 = Integer.parseInt(reader.readLine());
         int number2 = Integer.parseInt(reader.readLine());
         int number3 = Integer.parseInt(reader.readLine());
         int number4 = Integer.parseInt(reader.readLine());
         int number5 = Integer.parseInt(reader.readLine());
 
-        ArrayList<Integer> numbers = new ArrayList<>(); //создали лист
-        numbers.add(number1);
-        numbers.add(number2);
-        numbers.add(number3);
-        numbers.add(number4);
-        numbers.add(number5);
+//        ArrayList<Integer> numbers = new ArrayList<>(); //создали лист ArrayList
+//        numbers.add(number1);
+//        numbers.add(number2);
+//        numbers.add(number3);
+//        numbers.add(number4);
+//        numbers.add(number5);
+//        Collections.sort(numbers);
 
-        for (int i = 0; i < numbers.size(); i++) {      //выводим поочереди весь список
-            System.out.println(numbers.get(i));
+//        for (int i = 0; i < numbers.size(); i++) {      //выводим поочереди весь список ArrayList
+//            System.out.println(numbers.get(i));
+//        }
+
+////////////////////////////////////////////////////////////////
+        int numbers[] = new int[]{number1, number2, number3, number4, number5};       //через обычный массив
+        Arrays.sort(numbers);
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }

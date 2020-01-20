989bb0059fc025076cab4fbdc6b2c1d8b89fe68c
Moby812 <moby812@gmail.com>
2020-01-20 06:08:50 +0300

    Блокнот для новых идей

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0617/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task06.task0617;
+
+/* 
+Блокнот для новых идей
+1. В классе Solution создать public static класс Idea.
+2. В классе Idea создать метод public String getDescription(), который будет возвращать любую строку.
+3. В классе Solution создай статический метод public void printIdea(Idea idea), который будет выводить на экран описание идеи - это то, что возвращает метод getDescription().
+
+
+Требования:
+1. В классе Solution создать public static класс Idea.
+2. В классе Idea создать метод public String getDescription().
+3. Метод getDescription должен возвращать любую строку.
+4. В классе Solution создайте метод public static void printIdea(Idea idea).
+5. Метод printIdea должен выводить описание идеи на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printIdea(new Idea());
+    }
+
+    //напишите тут ваш код
+    public static class Idea{
+        public String getDescription(String x){
+            return x;
+        }
+    }
+}

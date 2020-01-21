2f69c6ddf94c23a28136a2f81f75a3ae6d81350e
Парамонов Павел <moby812@gmail.com>
2020-01-21 16:36:25 +0300

    Родственные связи кошек

--- 1.JavaSyntax/src/com/javarush/task/task06/task0621/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0621/Solution.java
@@ -6,25 +6,65 @@ import java.io.InputStreamReader;
 
 /* 
 Родственные связи кошек
+Задача: У каждой кошки есть имя и кошка-мама.
+Создать класс, который бы описывал данную ситуацию.
+Создать два объекта: кошку-дочь и кошку-маму.
+Вывести их на экран.
+
+Новая задача: У каждой кошки есть имя, кот-папа и кошка-мама.
+Изменить класс Cat так, чтобы он мог описать данную ситуацию.
+Создать 6 объектов: дедушку (папин папа), бабушку (мамина мама), папу, маму, сына, дочь.
+Вывести их всех на экран в порядке: дедушка, бабушка, папа, мама, сын, дочь.
+
+1. Программа должна считывать имена 6 котов в указанном порядке.
+2. Метод main должен создавать 6 объектов типа Cat.
+3. Программа должна выводить 6 строк с информацией о котах.
+4. Строка про дедушку (первая) должна соответствовать условию.
+5. Строка про бабушку (вторая) должна соответствовать условию.
+6. Строка про папу (третья) должна соответствовать условию.
+7. Строка про маму (четвертая) должна соответствовать условию.
+8. Строка про сына (пятая) должна соответствовать условию.
+9. Строка про дочь (шестая) должна соответствовать условию.
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String motherName = reader.readLine();
-        Cat catMother = new Cat(motherName);
+        String grandFatherName = reader.readLine();         //дедушка
+        Cat catGrandFather = new Cat(grandFatherName);
 
-        String daughterName = reader.readLine();
-        Cat catDaughter = new Cat(daughterName, catMother);
+        String grandMotherName = reader.readLine();         //бабушка
+        Cat catGrandMother = new Cat(grandMotherName);
 
-        System.out.println(catMother);
+        String fatherName = reader.readLine();              //папа (+ дедушка +бабушка)
+        Cat catFather = new Cat(fatherName, catGrandFather, catGrandMother);
+
+        String motherName = reader.readLine();              //мама (+ дедушка +бабушка)
+        Cat catMother = new Cat(motherName, catGrandFather, catGrandMother);
+
+        String daughterName = reader.readLine();            //дочь
+        Cat catDaughter = new Cat(daughterName, catGrandMother);
+
+//        String motherName = reader.readLine();
+//        Cat catMother = new Cat(motherName);
+//
+//        String daughterName = reader.readLine();
+//        Cat catDaughter = new Cat(daughterName, catMother);
+
+//новый код
+
+
+
+
+        System.out.println(catGrandMother);
         System.out.println(catDaughter);
     }
 
     public static class Cat {
         private String name;
         private Cat parent;
+        private Cat grandParent;
 
         Cat(String name) {
             this.name = name;
@@ -32,6 +72,12 @@ public class Solution {
 
         Cat(String name, Cat parent) {
             this.name = name;
+            this.parent = parent;
+        }
+
+        Cat(String name, Cat grandParent, Cat parent) {          //конструктор на 3 колена
+            this.name = name;
+            this.grandParent = grandParent;
             this.parent = parent;
         }
 

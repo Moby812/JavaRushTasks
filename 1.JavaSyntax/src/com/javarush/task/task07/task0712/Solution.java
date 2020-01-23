053f97d105a636c55c0c540313bbcf3dce6b63a7
Парамонов Павел <moby812@gmail.com>
2020-01-23 15:47:00 +0300

    Самые-самые

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task07.task0712;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Самые-самые
+
+1. Создай список строк.
+2. Добавь в него 10 строчек с клавиатуры.
+3. Узнай, какая строка в списке встретится раньше: самая короткая или самая длинная.
+Если таких строк несколько, то должны быть учтены самые первые из них.
+4. Выведи на экран строку из п.3. Должна быть выведена одна строка.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        ArrayList<String> strings = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) strings.add(i, reader.readLine());     //заполняем лист строками с клавы
+
+        int minSize = 0;
+        int maxSize = 0;
+
+
+    }
+}

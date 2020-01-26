661959ad87d3d1bd51a25ca9e174289ec24c7e25
Moby812 <moby812@gmail.com>
2020-01-26 12:38:44 +0300

    Слова в обратном порядке

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task07.task0714;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Слова в обратном порядке
+Введи с клавиатуры 5 слов в список строк. Удали 3 - ий элемент списка, и выведи оставшиеся элементы в обратном порядке.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> abs = new ArrayList<String>();
+
+        for (int i = 0; i < 5; i++) abs.add(i, reader.readLine());
+
+        abs.remove(2);
+
+        for (int i = abs.size() - 1; i > 0; i--) System.out.println(abs.get(i));
+    }
+}

3d162c6cd1a7ec0118432a455f2c118c58b0684a
Moby812 <moby812@gmail.com>
2020-01-26 10:46:26 +0300

    В 2 переменные считаем сумму значений из чётных/нечётных ячеек массива

--- 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -21,11 +21,19 @@ public class Solution {
         ArrayList<String> strings = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 10; i++) strings.add(i, reader.readLine());     //заполняем лист строками с клавы
-
-        int minSize = 0;
+        for (int i = 0; i < 4; i++) strings.add(i, reader.readLine());     //заполняем лист строками с клавы
+        // TODO: 26.01.2020 временно сделаю счётчик на 4 строки
+        int minSize = Integer.MAX_VALUE;
         int maxSize = 0;
 
-
+        for (int i = 0; i < strings.size(); i++) {
+            for (int j = 0; j < strings.size(); j++) {
+                int size = strings.get(j).length();
+                if (minSize > size) minSize = size;
+                if (maxSize < size) maxSize = size;
+            }
+            if (minSize == strings.get(i).length()) System.out.println(strings.get(i));
+            if (maxSize == strings.get(i).length()) System.out.println(strings.get(i));
+        }
     }
 }

2b0b6cc4fec3f2e92d56ff2428f46fb6a371e3db
Moby812 <moby812@gmail.com>
2020-01-26 16:56:54 +0300

    Проверка на упорядоченность

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Проверка на упорядоченность
+
+1. Введи с клавиатуры 10 слов в список строк.
+2. Определить, является ли список упорядоченным по возрастанию длины строки.
+3. В случае отрицательного ответа вывести на экран индекс первого элемента, нарушающего такую упорядоченность.
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> listString = new ArrayList<String>();
+
+        for (int i = 0; i < 10; i++) listString.add(i, reader.readLine());
+
+
+    }
+}
+

31769c13c27455ffb23602d5a73b845809ca9314
Moby812 <moby812@gmail.com>
2020-01-27 21:05:24 +0300

    Вводим строки, пока пользователь не введет строку "end".

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0722/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task07.task0722;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Это конец
+
+Создать список строк.
+Ввести строки с клавиатуры и добавить их в список.
+Вводить с клавиатуры строки, пока пользователь не введет строку "end". Саму строку "end" не учитывать.
+Вывести строки на экран, каждую с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //напишите тут ваш код
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; true; i++) {
+            String text = reader.readLine();
+            if (text.equals("end")) break;
+            else strings.add(text);
+        }
+
+        for (String x: strings) System.out.println(x);
+    }
+}

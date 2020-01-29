28e44db739f8a465e0b23d81c271461410aa2650
Moby812 <moby812@gmail.com>
2020-01-29 20:41:02 +0300

    Семейная перепись

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0724/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task07.task0724;
+
+/* 
+Семейная перепись
+Создай класс Human с полями имя(String), пол(boolean), возраст(int), отец(Human), мать(Human).
+Создай объекты и заполни их так, чтобы получилось: Два дедушки, две бабушки, отец, мать, трое детей. Вывести объекты на экран.
+
+Примечание:
+Если написать свой метод String toString() в классе Human, то именно он будет использоваться при выводе объекта на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // напишите тут ваш код
+
+    }
+
+    public static class Human {
+        // напишите тут ваш код
+
+        public String toString() {
+            String text = "";
+            text += "Имя: " + this.name;
+            text += ", пол: " + (this.sex ? "мужской" : "женский");
+            text += ", возраст: " + this.age;
+
+            if (this.father != null) {
+                text += ", отец: " + this.father.name;
+            }
+
+            if (this.mother != null) {
+                text += ", мать: " + this.mother.name;
+            }
+
+            return text;
+        }
+    }
+}

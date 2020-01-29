255e7dd47570043fb888bbd2eda3a59d2a4999cf
Moby812 <moby812@gmail.com>
2020-01-29 20:54:03 +0300

    Семейная перепись

--- 1.JavaSyntax/src/com/javarush/task/task07/task0724/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0724/Solution.java
@@ -12,11 +12,38 @@ package com.javarush.task.task07.task0724;
 public class Solution {
     public static void main(String[] args) {
         // напишите тут ваш код
-
+ Human grandPaOne = new Human();
+ Human grandPaTwo = new Human();
+ Human grandMaOne = new Human();
+ Human grandMaTwo = new Human();
+ Human pa = new Human();
+ Human ma = new Human();
+ Human childOne = new Human();
+ Human childTwo = new Human();
+ Human childThree = new Human();
     }
 
     public static class Human {
         // напишите тут ваш код
+        String name;
+        boolean sex;
+        int age;
+        Human father;
+        Human mother;
+
+        public Human(String name, boolean sex, int age) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        }
+
+        public Human(String name, boolean sex, int age, Human father, Human mother) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.father = father;
+            this.mother = mother;
+        }
 
         public String toString() {
             String text = "";

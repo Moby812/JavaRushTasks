b6f4800e5d88a62d15d54a25838928da7698da03
Парамонов Павел <moby812@gmail.com>
2020-01-30 16:34:46 +0300

    Коллекция Map из котов

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0803/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task08.task0803;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Коллекция Map из котов
+
+Есть класс Cat с полем имя (name, String).
+Создать коллекцию Map<String, Cat> (реализация HashMap).
+Добавить в коллекцию 10 котов, в качестве ключа использовать имя кота.
+Вывести результат на экран, каждый элемент с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String[] cats = new String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
+
+        Map<String, Cat> map = addCatsToMap(cats);
+
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+        }
+    }
+
+
+    public static Map<String, Cat> addCatsToMap(String[] cats) {
+        //напишите тут ваш код
+
+    }
+
+
+    public static class Cat {
+        String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name != null ? name.toUpperCase() : null;
+        }
+    }
+}

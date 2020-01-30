501d449e1ccca78927d09080bbae4b350894f947
Парамонов Павел <moby812@gmail.com>
2020-01-30 15:49:05 +0300

    Из коллекции Map<String, Object> (реализация HashMap) выводим её ключи и значение посредством Iterator'a

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0806/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task08.task0806;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Коллекция Map из Object
+Есть коллекция Map<String, Object> (реализация HashMap), туда занесли 10 различных пар объектов.
+Вывести содержимое коллекции на экран, каждый элемент с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("Sim", 5);
+        map.put("Tom", 5.5);
+        map.put("Arbus", false);
+        map.put("Baby", null);
+        map.put("Cat", "Cat");
+        map.put("Eat", new Long(56));
+        map.put("Food", new Character('3'));
+        map.put("Gevey", '6');
+        map.put("Hugs", 111111111111L);
+        map.put("Comp", (double) 123);
+
+        //напишите тут ваш код
+        for (Map.Entry<String, Object> pair: map.entrySet()) {
+            System.out.println(pair.getKey() +" "+ pair.getValue());
+        }
+
+    }
+}

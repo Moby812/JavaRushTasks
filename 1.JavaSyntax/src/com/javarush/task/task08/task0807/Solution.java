23b28bd0b848a8e25b3ef1d010dd94ed7e4316b6
Moby812 <moby812@gmail.com>
2020-02-01 14:03:09 +0300

    Создал два списка - LinkedList и ArrayList

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/* 
+LinkedList и ArrayList
+Нужно создать два списка - LinkedList и ArrayList.
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //напишите тут ваш код
+        ArrayList arrayList = new ArrayList();
+        return arrayList;
+    }
+
+    public static Object createLinkedList() {
+        //напишите тут ваш код
+        LinkedList linkedList = new LinkedList();
+        return linkedList;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

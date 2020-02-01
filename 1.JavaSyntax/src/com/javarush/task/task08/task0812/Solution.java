3b78a2a481a35ec4b42d0c445ee4a8d57bd3ddaf
Moby812 <moby812@gmail.com>
2020-02-01 18:59:14 +0300

    Находим самую длинную последовательность чисел в листе (через 2 листа)

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -18,12 +18,12 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         //напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        List<Integer> numbers = new ArrayList<>();                                      //создаём лист
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(2,2,3,3,4,4,5,5,5,2));      //быстрая инициализация переменных в лист
+        List<Integer> numbers = new ArrayList<>();                                                       //создаём лист
+//        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 4, 4, 4, 8, 8, 4, 12, 12, 14));      //быстрая инициализация переменных в лист
 
-//        for (int i = 0; i < 10; i++) numbers.add(Integer.parseInt(reader.readLine()));  //заполняем лист 10-ю значениями с клавиатуры
+        for (int i = 0; i < 10; i++) numbers.add(Integer.parseInt(reader.readLine()));                //заполняем лист 10-ю значениями с клавиатуры
 
-        List<Integer> countList = new ArrayList<>();
+        List<Integer> countList = new ArrayList<>();                                                //альтернатива без 2х листов = https://javarush.ru/help/35825
         int count = 1;
         for (int i = 0; i < numbers.size() - 1; i++) {
             if (numbers.get(i).equals(numbers.get(i + 1))) count += 1;

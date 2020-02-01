713f6a81677bf419fc39ed6664f465a378c761b2
Moby812 <moby812@gmail.com>
2020-02-01 15:11:23 +0300

    Самая длинная последовательность

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task08.task0812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Cамая длинная последовательность
+1. Создай список чисел.
+2. Добавь в список 10 чисел с клавиатуры.
+3. Вывести на экран длину самой длинной последовательности повторяющихся чисел в списке.
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> numbers = new ArrayList<>();                                      //создаём лист
+
+        for (int i = 0; i < 10; i++) numbers.add(Integer.parseInt(reader.readLine()));  //заполняем лист 10-ю значениями с клавиатуры
+
+    }
+}

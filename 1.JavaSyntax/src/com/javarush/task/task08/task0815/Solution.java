47c5371ed22b0ae4642a798859eb8861a5b946a6
Moby812 <moby812@gmail.com>
2020-02-02 14:33:49 +0300

    черновик

--- 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
@@ -12,12 +12,25 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> createMap() {
         //напишите тут ваш код
-//    Map<String, String> fio = new
+        Map<String, String> fio = new HashMap<>();
+        fio.put("Парамонов", "Павел");
+        fio.put("Гончарова", "Екатерина");
+        fio.put("Овчинников", "Илья");
+        fio.put("Камчатнов", "Кирилл");
+        fio.put("Уланов", "Денис");
+        fio.put("Басов", "Илья");
+        fio.put("Рачков", "Александр");
+        fio.put("Шатров", "Алексей");
+        fio.put("Наумкин", "Михаил");
+        fio.put("Якушин", "Алексей");
+        return fio;
     }
 
     public static int getCountTheSameFirstName(Map<String, String> map, String name) {
         //напишите тут ваш код
-
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            String fioName = pair.getValue();
+        }
     }
 
     public static int getCountTheSameLastName(Map<String, String> map, String lastName) {

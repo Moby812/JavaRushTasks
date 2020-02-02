de4a9e3ba6722a09c1ae6bf8d9dd80cbd842db70
Moby812 <moby812@gmail.com>
2020-02-02 14:23:48 +0300

    Перепись населения

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task08.task0815;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Перепись населения
+Создать словарь (Map<String, String>) занести в него десять записей по принципу "Фамилия" - "Имя".
+Проверить сколько людей имеют совпадающие с заданным именем или фамилией.
+*/
+
+public class Solution {
+    public static Map<String, String> createMap() {
+        //напишите тут ваш код
+//    Map<String, String> fio = new
+    }
+
+    public static int getCountTheSameFirstName(Map<String, String> map, String name) {
+        //напишите тут ваш код
+
+    }
+
+    public static int getCountTheSameLastName(Map<String, String> map, String lastName) {
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

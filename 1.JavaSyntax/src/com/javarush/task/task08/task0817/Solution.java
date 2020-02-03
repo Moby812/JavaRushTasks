70f7eda6fa1055135370565b8ab9ea89f088eb62
Парамонов Павел <moby812@gmail.com>
2020-02-03 09:54:17 +0300

    Коллекция Map из котов

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task08.task0817;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Нам повторы не нужны
+Создать словарь (Map<String, String>) занести в него десять записей по принципу "фамилия" - "имя".
+Удалить людей, имеющих одинаковые имена.
+*/
+
+public class Solution {
+    public static Map<String, String> createMap() {
+        //напишите тут ваш код
+        Map<String, String> fio = new HashMap<>();
+        fio.put("Парамонов","Павел");
+        fio.put("Гончарова", "Екатерина");
+        fio.put("Овчинников", "Илья");
+        fio.put("Камчатнов", "Кирилл");
+        fio.put("Уланов", "Денис");
+        fio.put("Басов", "Илья");
+        fio.put("Рачков", "Александр");
+        fio.put("Шатров", "Алексей");
+        fio.put("Наумкин", "Михаил");
+        fio.put("Якушин", "Алексей");
+    return fio;
+    }
+
+    public static void removeTheFirstNameDuplicates(Map<String, String> map) {
+        //напишите тут ваш код
+        removeItemFromMapByValue(createMap(), "Алексей");
+
+    }
+
+    public static void removeItemFromMapByValue(Map<String, String> map, String value) {
+        Map<String, String> copy = new HashMap<>(map);
+        for (Map.Entry<String, String> pair : copy.entrySet()) {
+            if (pair.getValue().equals(value)) {
+                map.remove(pair.getKey());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//for (Map.Entry<String, String> pair: )
+    }
+}

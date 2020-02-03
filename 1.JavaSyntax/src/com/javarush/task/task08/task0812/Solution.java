533e7f0a119e37f2e1732c187386c4545eccaf98
Парамонов Павел <moby812@gmail.com>
2020-02-03 08:30:02 +0300

    Самая длинная последовательность

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -17,22 +17,38 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) throws IOException {
         //напишите тут ваш код
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> numbers = new ArrayList<>();                                                       //создаём лист
-//        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 4, 4, 4, 8, 8, 4, 12, 12, 14));      //быстрая инициализация переменных в лист
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        List<Integer> numbers = new ArrayList<>();                                                       //создаём лист
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(4, 4, 4, 8, 8, 8, 8, 8, 8, 8));      //быстрая инициализация переменных в лист
 
-        for (int i = 0; i < 10; i++) numbers.add(Integer.parseInt(reader.readLine()));                //заполняем лист 10-ю значениями с клавиатуры
+//        for (int i = 0; i < 10; i++) numbers.add(Integer.parseInt(reader.readLine()));                //заполняем лист 10-ю значениями с клавиатуры
 
         List<Integer> countList = new ArrayList<>();                                                //альтернатива без 2х листов = https://javarush.ru/help/35825
         int count = 1;
-        for (int i = 0; i < numbers.size() - 1; i++) {
-            if (numbers.get(i).equals(numbers.get(i + 1))) count += 1;
+        for (int i = 0; i < numbers.size() ; i++) {
+            if (numbers.get(i).equals(numbers.get(i + 1))) count++;
+            else if (i+1 == numbers.size()-1) {
+                countList.add(count);
+            }
             else {
                 countList.add(count);
                 count = 1;
             }
         }
         Collections.sort(countList);
-        System.out.println(countList.get(countList.size()-1));                                  //должен вывести "3"
+        for (Integer x: countList) System.out.println(x);
+        System.out.println("Максимальная длина: "+ countList.get(countList.size()-1));                                  //должен вывести "3"
+
+//        int count = 0;
+//        int countMax = 1;
+//        for (int i = 0; i < numbers.size() - 1; i++) {
+//            if (numbers.get(i + 1).equals(numbers.get(i))) {
+//                count++;
+//                if (countMax < count) countMax = count;
+//            } else {
+//                count = 1;
+//            }
+//        }
+//        System.out.println(countMax);
     }
 }

c24e148f72f15da3d30dc3b7add9747766116068
Moby812 <moby812@gmail.com>
2020-02-03 20:27:16 +0300

    Добрая Зинаида и летние каникулы

--- 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
@@ -18,6 +18,16 @@ public class Solution {
         map.put("Сталлоне", dateFormat.parse("MAY 1 2012"));
 
         //напишите тут ваш код
+        map.put("Павел", dateFormat.parse("APR 4 1989"));
+        map.put("Катерина", dateFormat.parse("AUG 20 1994"));
+        map.put("Олег", dateFormat.parse("AUG 7 2019"));
+        map.put("Юлия", dateFormat.parse("DEC 31 1964"));
+        map.put("Ленин", dateFormat.parse("APR 22 1870"));
+        map.put("Сталин", dateFormat.parse("DEC 18 1878"));
+        map.put("Эйнштейн", dateFormat.parse("MAR 14 1879"));
+        map.put("Никола", dateFormat.parse("YUL 10 1856"));
+        map.put("Чарльз", dateFormat.parse("FEB 12 1809"));
+    return map;
     }
 
     public static void removeAllSummerPeople(Map<String, Date> map) {

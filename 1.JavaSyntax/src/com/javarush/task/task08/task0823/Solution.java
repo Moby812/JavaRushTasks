1be7f0b1cff460512105a02f4b1de4d5e6a8ab7c
Парамонов Павел <moby812@gmail.com>
2020-02-04 15:53:17 +0300

    Омовение Рамы

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task08.task0823;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Омовение Рамы
+Написать программу, которая вводит с клавиатуры строку текста.
+Программа заменяет в тексте первые буквы всех слов на заглавные.
+Вывести результат на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+
+        //напишите тут ваш код
+    }
+}

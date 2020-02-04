bd5535c376b1b73bb79bf1583f804148d4defc46
Парамонов Павел <moby812@gmail.com>
2020-02-04 15:41:42 +0300

    Минимальное из N чисел

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task08.task0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Минимальное из N чисел
+1. Ввести с клавиатуры число N.
+2. Считать N целых чисел и заполнить ими список - метод getIntegerList.
+3. Найти минимальное число среди элементов списка - метод getMinimum.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        // Найти минимум тут
+        return 0;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        // Создать и заполнить список тут
+
+        return null;
+    }
+}

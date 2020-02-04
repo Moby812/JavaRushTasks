deaa3e4388e89f5496294af46545592e09d2b398
Парамонов Павел <moby812@gmail.com>
2020-02-04 16:28:36 +0300

    Омовение Рамы

--- 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.task.task08.task0823;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /* 
 Омовение Рамы
@@ -17,5 +20,13 @@ public class Solution {
         String string = reader.readLine();
 
         //напишите тут ваш код
+        List <String> words = new ArrayList<>(Arrays.asList(string.split(" ")));                                  //парсим строку по условию " ", и добавляем строки в создаваемый массив words
+
+//        List <String> words = new ArrayList<>();
+//        String word[] = string.split(" ");
+
+
+        for (String x: words) System.out.println(x);
+
     }
 }

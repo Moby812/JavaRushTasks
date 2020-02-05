aa0016dbca54b21c5c5623d653065ccfe1443fa3
Парамонов Павел <moby812@gmail.com>
2020-02-05 15:38:51 +0300

    Номер месяца

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0828/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task08.task0828;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/* 
+Номер месяца
+1. Программа должна считывать одно значение с клавиатуры.
+2. Программа должна выводить текст на экран.
+3. Программа должна использовать коллекции.
+4. Программа должна считывать с клавиатуры имя месяца и выводить его номер на экран по заданному шаблону.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+    }
+}

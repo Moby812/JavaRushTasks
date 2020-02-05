3f26c96416aa1c195170ec59c6ae37ae19591414
Парамонов Павел <moby812@gmail.com>
2020-02-05 14:34:35 +0300

    Пять победителей. Пузырьком перебераем массив.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task08.task0826;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 
 /* 
 Пять победителей
@@ -15,11 +16,11 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        int[] array = new int[20];                                  //step 1
-        int[] array = {2,6,7,1,0,23,18};           // TODO: 2020-02-05 Временная заглушка
-//        for (int i = 0; i < array.length; i++) {
-//            array[i] = Integer.parseInt(reader.readLine());         //step 2
-//        }
+        int[] array = new int[20];                                  //step 1
+//        int[] array = {2,6,7,1,0,23,18};           // TODO: 2020-02-05 Временная заглушка
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());         //step 2
+        }
 
         sort(array);                                                                                                    //сортируем на уменьшение
 
@@ -31,11 +32,14 @@ public class Solution {
     }
 
     public static void sort(int[] array) {
+//    public static void sort(Integer[] array) {
         //напишите тут ваш код
-//        Arrays.sort(array);           //быстрая обратная сортировка
+//        Arrays.sort(array);                                     //быстрая прямая сортировка
 
-        for (int i = 0; i < array.length-1; i++) {
-            for (int j = array.length-2; j >= 0; j--) {
+//        Arrays.sort(array, Collections.reverseOrder());         //быстрая обратная сортировка работает только с типом переменной Integer
+
+        for (int i = array.length-1; i > 0; i--) {                 //сортировка пузырьком
+            for (int j = 0; j < i; j++) {
                 int tmp;
                 if (array[j] < array[j+1]) {
                     tmp = array[j];

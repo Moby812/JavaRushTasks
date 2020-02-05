cf57370612d69c7c49d536bc4aa9a00510cba2ab
Парамонов Павел <moby812@gmail.com>
2020-02-05 14:12:45 +0300

    Пять победителей. Пузырьком перебераем массив.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task08.task0826;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* 
 Пять победителей
@@ -14,10 +15,11 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = new int[20];                                  //step 1
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(reader.readLine());         //step 2
-        }
+//        int[] array = new int[20];                                  //step 1
+        int[] array = {2,6,7,1,0,23,18};           // TODO: 2020-02-05 Временная заглушка
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = Integer.parseInt(reader.readLine());         //step 2
+//        }
 
         sort(array);                                                                                                    //сортируем на уменьшение
 
@@ -30,7 +32,17 @@ public class Solution {
 
     public static void sort(int[] array) {
         //напишите тут ваш код
+//        Arrays.sort(array);           //быстрая обратная сортировка
 
-
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = array.length-2; j >= 0; j--) {
+                int tmp;
+                if (array[j] < array[j+1]) {
+                    tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
     }
 }

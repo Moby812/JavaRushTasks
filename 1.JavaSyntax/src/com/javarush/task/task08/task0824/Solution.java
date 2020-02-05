e7adc4436743a454a335742ba2039fb3aab92aa8
Парамонов Павел <moby812@gmail.com>
2020-02-05 11:18:18 +0300

    Собираем семейство

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0824/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task08.task0824;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Собираем семейство
+1. Создай класс Human с полями имя (String), пол (boolean), возраст (int), дети (ArrayList<Human>).
+2. Создай объекты и заполни их так, чтобы получилось: два дедушки, две бабушки, отец, мать, трое детей.
+3. Выведи все объекты Human на экран (Подсказка: используй метод toString() класса Human).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+
+    public static class Human {
+        //напишите тут ваш код
+        String name;
+        boolean sex;
+        int age;
+        ArrayList<Human> children;
+
+        public String toString() {
+            String text = "";
+            text += "Имя: " + this.name;
+            text += ", пол: " + (this.sex ? "мужской" : "женский");
+            text += ", возраст: " + this.age;
+
+            int childCount = this.children.size();
+            if (childCount > 0) {
+                text += ", дети: " + this.children.get(0).name;
+
+                for (int i = 1; i < childCount; i++) {
+                    Human child = this.children.get(i);
+                    text += ", " + child.name;
+                }
+            }
+            return text;
+        }
+    }
+}

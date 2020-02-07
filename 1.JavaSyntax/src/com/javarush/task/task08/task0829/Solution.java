8f84ec32adcc0e9928388533218eb60c5c204cab
Парамонов Павел <moby812@gmail.com>
2020-02-07 12:05:57 +0300

    Модернизация ПО

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0829;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Модернизация ПО
+
+Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
+Новая задача: Программа должна работать не с номерами домов, а с городами:
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = new ArrayList<>();
+        while (true) {
+            String family = reader.readLine();
+            if (family.isEmpty()) {
+                break;
+            }
+
+            list.add(family);
+        }
+
+        // Read the house number
+        int houseNumber = Integer.parseInt(reader.readLine());
+
+        if (0 <= houseNumber && houseNumber < list.size()) {
+            String familyName = list.get(houseNumber);
+            System.out.println(familyName);
+        }
+    }
+}

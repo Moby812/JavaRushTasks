75b9a76428674f757a0fecf83a5105e5c2a8b767
Парамонов Павел <moby812@gmail.com>
2020-02-07 12:31:59 +0300

    Задача по алгоритмам

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0830/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task08.task0830;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам
+Задача: Введи с клавиатуры 20 слов и выведи их в алфавитном порядке. Каждое слово - с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String word : array) {
+            System.out.println(word);
+        }
+    }
+
+    public static void sort(String[] array) {
+        //напишите тут ваш код
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}

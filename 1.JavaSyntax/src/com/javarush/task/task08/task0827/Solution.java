7fe076070401c8d31f4338239799c7bcce1768c5
Парамонов Павел <moby812@gmail.com>
2020-02-10 10:36:55 +0300

    Работа с датой

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task08.task0827;
+
+import java.util.Date;
+
+/* 
+Работа с датой
+
+1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true, если количество дней с начала года - нечетное число, иначе false
+2. String date передается в формате FEBRUARY 1 2013
+Не забудьте учесть первый день года.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isDateOdd("MAY 1 2013"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        return true;
+    }
+}
